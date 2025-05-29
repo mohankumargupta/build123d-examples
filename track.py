32571c307b00f1b3283999cff70596b0239c05b7
Mohan Gupta <mohangupta@live.com>
2025-05-30 09:43:58 +1000

    base track from h

--- track.py
+++ track.py
@@ -3,11 +3,21 @@
 from build123d import *
 from ocp_vscode import show_all, Camera
 
+# Parameters(mm)
+
+## H base shape ##
 length = 67.85
 width = 28.57
 height = 1.72
 notch_width = 9.14
 notch_depth = 3
+
+## Tubes ##
+tube_radius = 1
+long_tube_length = 54.25
+short_tube_length = 25.45
+long_x_offset = tube_radius + (2.65 / 2)
+short_x_offset = tube_radius + (19.17 / 2)
 
 with BuildPart() as builder:
     with BuildSketch() as sketch1:
