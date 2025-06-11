7b481709a09f93cfcdc4254f98f5a30624ac728e
Mohan Gupta <mohangupta@live.com>
2025-06-11 12:55:20 +1000

    sweep works

--- examples/box-lid.py
+++ examples/box-lid.py
@@ -7,17 +7,33 @@ from ocp_vscode import show, show_all, Camera
 length=155
 width=45
 bottom_thickness=2
+thickness = 2
 
 #Radius and diameters(mm)
 corner_radius = 4
 
 with BuildPart() as builder:
     with BuildSketch(Plane.XY) as sketch1:
-        RectangleRounded(length, width, radius=corner_radius)
+        rr = RectangleRounded(length, width, radius=corner_radius)
     extrude(amount=bottom_thickness)
+    with BuildSketch(Plane.XZ) as sketch2:
+        with BuildLine() as line1:
+            Polyline(
+                (-(length/2+thickness), 0),
+                (-(length/2+thickness), 30),
+                (-length/2, 30),
+                (-length/2, 31),
+                (-(length/2 - 1), 31),
+                (-(length/2 - 1), 27),
+                (-length/2, 26),
+                (-length/2, 0),
+                close=True
+            )
+        make_face()
+    sweep(path=rr.wire())
 
 part = builder.part
 
-#show(builder, reset_camera=Camera.KEEP)
-show_all(reset_camera=Camera.KEEP)
+show(builder, sketch2, reset_camera=Camera.KEEP)
+#show_all(reset_camera=Camera.KEEP)
 print(part.volume)
