99897b3d9b5b0fb36cea07be64f8de62266b2509
Mohan Gupta <mohangupta@live.com>
2025-06-14 16:48:03 +1000

    knob: not right

--- examples/knob.py
+++ examples/knob.py
@@ -16,8 +16,20 @@ with BuildPart() as builder:
     rim_face = builder.faces().sort_by(Axis.Z)[-2]
     rim = rim_face.outer_wire().edge()
     chamfer(rim, 1)
+    with BuildSketch(Plane.XZ.shift_origin((0,0, 10.4))) as sketch2:
+        with BuildLine() as line2:
+            l1 = Line((-9,0), (-9, -5.35))
+            l2 = PolarLine(start=l1@1, length=20, angle=(90-18), length_mode=LengthMode.DIAGONAL, mode=Mode.PRIVATE)
+            p1 = l2.find_intersection_points(Axis.X)[0]
+            print(p1)
+            l3 = Line(l1@1,(p1.X, 0))
+            Line(l3@1, l1@0)
+        make_face()
+    revolve(axis=Axis.Z, mode=Mode.SUBTRACT)
+        #IntersectingLine(l1@1, direction=(l2%1), other=l2)
+        
 #show(sketch1, show_sketch_local=True ,reset_camera=Camera.KEEP)
-show(builder.part, reset_camera=Camera.KEEP)
+show(builder.part, sketch2, reset_camera=Camera.KEEP)
 #show(rim_face, show_parent=True)
 #show_all(reset_camera=Camera.KEEP)
      
@@ -59,3 +71,27 @@ HTML(svg_example1)
 
 
 
+#%%
+
+# from build123d import *
+# from ocp_vscode import show, show_all, Camera
+
+# with BuildPart() as test:
+#     with BuildSketch(Plane.YZ) as two:
+#         with Locations((0, 45)):
+#             Circle(15)
+#         with BuildLine() as bl:
+#             c = Line((75 / 2, 0), (75 / 2, 60), mode=Mode.PRIVATE)
+#             u = two.edge().find_tangent(75 / 2 + 90)[0]  # where is the slope 75/2?
+#             print(two.edge().tangent_at(u))
+#             l1 = IntersectingLine(
+#                 two.edge().position_at(u), -two.edge().tangent_at(u), other=c
+#             )
+#             Line(l1 @ 0, (0, 45))
+#             Polyline((0, 0), c @ 0, l1 @ 1)
+#             mirror(about=Plane.YZ)
+#         make_face()
+#         # with Locations((0, 45)):
+#         #     Circle(12 / 2, mode=Mode.SUBTRACT)
+#     extrude(amount=-13)
+# show(test.part,c,two, reset_camera=Camera.KEEP)
