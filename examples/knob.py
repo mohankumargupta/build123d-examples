f2f02b88ca39899e0c3f2729f676c52bdd3e436f
Mohan Gupta <mohangupta@live.com>
2025-06-14 17:29:32 +1000

    knob: needs followup

--- examples/knob.py
+++ examples/knob.py
@@ -26,14 +26,73 @@ with BuildPart() as builder:
             Line(l3@1, l1@0)
         make_face()
     
-    revolve(axis=Axis(origin=(-9,0), direction=(0,0,1)), mode=Mode.SUBTRACT)
+    r = revolve(axis=Axis(origin=(-9,0), direction=(0,0,1)), mode=Mode.SUBTRACT)
+    with PolarLocations(radius=9, count=12) as locs:
+        for loc in locs:
+            print(loc)
+    
         #IntersectingLine(l1@1, direction=(l2%1), other=l2)
         
 #show(sketch1, show_sketch_local=True ,reset_camera=Camera.KEEP)
 show(builder.part, reset_camera=Camera.KEEP)
 #show(rim_face, show_parent=True)
 #show_all(reset_camera=Camera.KEEP)
-     
+
+"""
+#%%
+
+from build123d import *
+from ocp_vscode import show, show_all, Camera
+
+with BuildPart() as builder:
+    # ------------------ 1. BASE PART (Unchanged) -------------------
+    with BuildSketch(Plane.XZ) as sketch1:
+        with BuildLine():
+            Polyline((0,4.6), (0,10.4), (9, 10.4), (6,0),(2.8,0),(2.8, 4.6), close=True)
+        make_face()
+        with Locations((0, 10.4)):
+            Circle(2, mode=Mode.SUBTRACT)
+        with Locations((5, 10.4)):
+            Circle(1, mode=Mode.SUBTRACT)
+    revolve(axis=Axis.Z)
+    rim_face = builder.faces().sort_by(Axis.Z)[-2]
+    rim = rim_face.outer_wire().edge()
+    chamfer(rim, 1)
+
+    # ------------------ 2. DEFINE A LOCAL CUTTER TEMPLATE -------------------
+    # Create the cutting sketch once, defined locally around the origin (0,0).
+    # This sketch is our "common sketch".
+    with BuildSketch(Plane.XZ) as cutter_template:
+        with BuildLine(mode=Mode.PRIVATE) as helper_line:
+            # Geometry is shifted from x=-9 to x=0 to make it local
+            start_pt = (0, 10.4 - 5.35)
+            PolarLine(start_pt, length=20, angle=180 - (90-18)) # Angle points "inward"
+
+        # Find intersection with the local top plane (z=10.4)
+        top_plane = Plane.XY.offset(10.4)
+        intersect_point = top_plane.intersect(helper_line.edges()[0])
+
+        # Create the final triangular cutting face from local points
+        Polyline(
+            (0, 10.4),
+            intersect_point,
+            (0, 10.4 - 5.35),
+            close=True
+        )
+        make_face()
+
+    # ------------------ 3. PATTERN THE CUT -------------------
+    # Use PolarLocations to define the 12 positions
+    with PolarLocations(radius=9, count=12):
+        # At each location, create a revolved part from the template and add it.
+        # The mode=Mode.SUBTRACT on the revolve operation ensures it cuts away material.
+        # The axis=Axis.Z is now LOCAL to each of the 12 positions.
+        add(revolve(cutter_template.sketch, axis=Axis.Z, mode=Mode.SUBTRACT))
+
+
+show(builder.part, reset_camera=Camera.KEEP)
+"""    
+
 
 #%%
 
